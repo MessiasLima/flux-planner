61b7268692549ed2374c77900a7714c2c68c4796
Messias Junior <messiaslima.03@gmail.com>
2022-08-02 10:07:42 +0100

    chore: remove unused shapes

--- common/src/commonMain/kotlin/io/appoutlet/flux/common/core/ui/Shapes.kt
+++ common/src/commonMain/kotlin/io/appoutlet/flux/common/core/ui/Shapes.kt
@@ -1,9 +1,8 @@
 package io.appoutlet.flux.common.core.ui
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.unit.dp
 
-val shapeSmall = RoundedCornerShape(4.dp)
+// val shapeSmall = RoundedCornerShape(4.dp)
 val shapeMedium = RoundedCornerShape(14.dp)
-val shapeLarge = RoundedCornerShape(18.dp)
+// val shapeLarge = RoundedCornerShape(18.dp)
