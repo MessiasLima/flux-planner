473ea6ebcefa58db8f894e567567c0e531e51613
messiaslima <messiaslima.03@gmail.com>
2022-08-10 15:50:06 +0100

    refactor: remove wildcard imports

--- desktop/src/jvmMain/kotlin/io/appoutlet/flux/desktop/feature/signin/SignInView.kt
+++ desktop/src/jvmMain/kotlin/io/appoutlet/flux/desktop/feature/signin/SignInView.kt
@@ -1,13 +1,24 @@
 package io.appoutlet.flux.desktop.feature.signin
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -35,7 +46,7 @@ fun SignInView(
 }
 
 @Composable
-fun SignInForm(viewModel: SignInViewModel, karavel: Karavel?, onLoginSuccessful: () -> Unit) {
+private fun SignInForm(viewModel: SignInViewModel, karavel: Karavel?, onLoginSuccessful: () -> Unit) {
     Box {
         val uiState: SignInUiState by viewModel.uiState.collectAsState(initial = SignInUiState.Idle)
         val isLoading = uiState is SignInUiState.Loading
--- desktop/src/jvmMain/kotlin/io/appoutlet/flux/desktop/feature/signin/component/PasswordTextField.kt
+++ desktop/src/jvmMain/kotlin/io/appoutlet/flux/desktop/feature/signin/component/PasswordTextField.kt
@@ -9,7 +9,11 @@ import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
