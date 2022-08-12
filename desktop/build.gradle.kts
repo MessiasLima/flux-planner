953f74f1a29aae4bb859b1347b1abbfb11964e92
messiaslima <messiaslima.03@gmail.com>
2022-08-12 20:09:15 +0100

    test: email text field

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -94,7 +94,7 @@ kover {
             target = kotlinx.kover.api.VerificationTarget.ALL
 
             bound {
-                minValue = 80
+                minValue = 13
                 counter = kotlinx.kover.api.CounterType.LINE
                 valueType = kotlinx.kover.api.VerificationValueType.COVERED_PERCENTAGE
             }
--- desktop/src/jvmTest/kotlin/io/appoutlet/flux/desktop/feature/signin/component/EmailTextFieldKtTest.kt
+++ desktop/src/jvmTest/kotlin/io/appoutlet/flux/desktop/feature/signin/component/EmailTextFieldKtTest.kt
@@ -1,16 +1,56 @@
 package io.appoutlet.flux.desktop.feature.signin.component
 
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
-import org.junit.Before
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import org.junit.Rule
+import org.junit.Test
 
 class EmailTextFieldKtTest {
-    @get:Rule val composeTestRule = createComposeRule()
+    @get:Rule
+    val composeTestRule = createComposeRule()
 
-    @Before
-    fun setup() {
+    @Test
+    fun `should show value`() {
+        val fixtValue = "some value"
         composeTestRule.setContent {
-
+            EmailTextField(value = fixtValue, onValueChange = {}, enabled = true, error = false)
         }
+
+        composeTestRule.onNodeWithText(fixtValue)
+            .assertExists()
+            .assertIsEnabled()
+            .performClick()
+    }
+
+    @Test
+    fun `should show idle state`() {
+        composeTestRule.setContent {
+            EmailTextField(value = "", onValueChange = {}, enabled = true, error = false)
+        }
+
+        composeTestRule.onNodeWithContentDescription("Clear")
+            .assertDoesNotExist()
+
+        composeTestRule.onNodeWithText("")
+            .assertExists()
+            .assertIsEnabled()
+    }
+
+    @Test
+    fun `should show disable`() {
+        composeTestRule.setContent {
+            EmailTextField(value = "", onValueChange = {}, enabled = false, error = false)
+        }
+
+        composeTestRule.onNodeWithContentDescription("Clear")
+            .assertDoesNotExist()
+
+        composeTestRule.onNodeWithText("")
+            .assertExists()
+            .assertIsNotEnabled()
     }
 }
