ecc35ef86a41b4ef10fa936db0674ded3d183d22
messiaslima <messiaslima.03@gmail.com>
2022-08-12 18:54:05 +0100

    build: cleanup kover setup for common

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -124,34 +124,29 @@ val excludePatterns = listOf(
 )
 
 kover {
-    isDisabled.set(false) // true to disable instrumentation and all Kover tasks in this project
-    engine.set(kotlinx.kover.api.DefaultIntellijEngine) // change Coverage Engine
-    filters { // common filters for all default Kover tasks
-        classes { // common class filter for all default Kover tasks
-            includes += includePatterns // class inclusion rules
-            excludes += excludePatterns // class exclusion rules
+    isDisabled.set(false)
+    engine.set(kotlinx.kover.api.DefaultIntellijEngine)
+    filters {
+        classes {
+            includes += includePatterns
+            excludes += excludePatterns
         }
     }
 
     verify {
-        onCheck.set(true) // true to run koverVerify task during the execution of the check task
+        onCheck.set(true)
         isDisabled.set(false)
-        rule { // add verification rule
-            isEnabled = true // false to disable rule checking
-            name = null // custom name for the rule
-            target = kotlinx.kover.api.VerificationTarget.ALL // specify by which entity the code for separate coverage evaluation will be grouped
+        rule {
+            isEnabled = true
+            name = null
+            target = kotlinx.kover.api.VerificationTarget.ALL
 
             bound { // add rule bound
                 minValue = 80
-                counter = kotlinx.kover.api.CounterType.LINE // change coverage metric to evaluate (LINE, INSTRUCTION, BRANCH)
-                valueType = kotlinx.kover.api.VerificationValueType.COVERED_PERCENTAGE // change counter value (COVERED_COUNT, MISSED_COUNT, COVERED_PERCENTAGE, MISSED_PERCENTAGE)
+                counter = kotlinx.kover.api.CounterType.LINE
+                valueType = kotlinx.kover.api.VerificationValueType.COVERED_PERCENTAGE
             }
         }
     }
 }
-
-koverMerged {
-    enable()
-}
-
 // endregion
