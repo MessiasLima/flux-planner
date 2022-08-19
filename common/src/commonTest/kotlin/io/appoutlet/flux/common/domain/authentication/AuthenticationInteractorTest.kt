28aa82aa80e85a51871f513bebbff84176a8b9c9
messiaslima <messiaslima.03@gmail.com>
2022-08-19 08:56:00 +0100

    test: update Authentication interactor test

--- common/src/commonTest/kotlin/io/appoutlet/flux/common/domain/authentication/AuthenticationInteractorTest.kt
+++ common/src/commonTest/kotlin/io/appoutlet/flux/common/domain/authentication/AuthenticationInteractorTest.kt
@@ -3,10 +3,17 @@ package io.appoutlet.flux.common.domain.authentication
 import io.appoutlet.flux.common.core.network.authentication.AuthenticationApi
 import io.appoutlet.flux.common.core.network.authentication.AuthenticationRequest
 import io.appoutlet.flux.common.core.network.authentication.AuthenticationResponse
+import io.appoutlet.flux.common.core.network.authentication.SendEmailConfirmationRequest
+import io.appoutlet.flux.common.core.network.authentication.SendEmailConfirmationResponse
+import io.appoutlet.flux.common.core.network.authentication.SignUpWithEmailRequest
+import io.appoutlet.flux.common.core.network.authentication.SignUpWithEmailResponse
+import io.appoutlet.flux.common.core.network.authentication.UpdateProfileRequest
+import io.appoutlet.flux.common.core.network.authentication.UpdateProfileResponse
 import io.appoutlet.flux.common.domain.user.UserDomain
 import io.appoutlet.flux.common.domain.user.UserDomainMapper
 import io.appoutlet.flux.common.test.UnitTest
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -60,5 +67,68 @@ class AuthenticationInteractorTest : UnitTest<AuthenticationInteractor>() {
         ).first()
 
         assertEquals(fixtUserDomain, actual)
+    }
+
+    @Test
+    fun `should create user`() = runTest {
+        val fixtEmail: String = fixture()
+        val fixtPassword: String = fixture()
+        val fixtSignUpWithEmailRequest: SignUpWithEmailRequest = fixture()
+        val fixtSignUpWithEmailResponse: SignUpWithEmailResponse = fixture()
+        val fixtUserDomain: UserDomain = fixture()
+
+        every {
+            mockSignUpWithEmailRequestMapper.map(
+                email = fixtEmail,
+                password = fixtPassword
+            )
+        } returns fixtSignUpWithEmailRequest
+
+        coEvery { mockAuthenticationApi.signUpWithEmail(fixtSignUpWithEmailRequest) } returns fixtSignUpWithEmailResponse
+
+        every { mockUserDomainMapper.map(fixtSignUpWithEmailResponse) } returns fixtUserDomain
+
+        val actual = sut.createUser(email = fixtEmail, password = fixtPassword).first()
+
+        assertEquals(actual, fixtUserDomain)
+    }
+
+    @Test
+    fun `should update display name`() = runTest {
+        val fixtUserDomain: UserDomain = fixture()
+        val fixtDisplayName: String = fixture()
+        val fixtUpdateProfileRequest: UpdateProfileRequest = fixture()
+        val fixtUpdateProfileResponse: UpdateProfileResponse = fixture()
+
+        every {
+            mockUpdateProfileRequestMapper.map(
+                user = fixtUserDomain,
+                displayName = fixtDisplayName
+            )
+        } returns fixtUpdateProfileRequest
+
+        coEvery { mockAuthenticationApi.updateProfile(fixtUpdateProfileRequest) } returns fixtUpdateProfileResponse
+
+        sut.updateDisplayName(user = fixtUserDomain, name = fixtDisplayName)
+
+        coVerify(exactly = 1) { mockAuthenticationApi.updateProfile(fixtUpdateProfileRequest) }
+    }
+
+    @Test
+    fun `should email confirmation`() = runTest {
+        val fixtUserDomain: UserDomain = fixture()
+        val fixtSendEmailConfirmationRequest: SendEmailConfirmationRequest = fixture()
+        val fixtSendEmailConfirmationResponse: SendEmailConfirmationResponse = fixture()
+
+        every { mockSendEmailConfirmationRequestMapper.map(fixtUserDomain) } returns fixtSendEmailConfirmationRequest
+        coEvery { mockAuthenticationApi.sendEmailConfirmation(fixtSendEmailConfirmationRequest) } returns fixtSendEmailConfirmationResponse
+
+        sut.sendEmailConfirmation(fixtUserDomain)
+
+        coVerify(exactly = 1) {
+            mockAuthenticationApi.sendEmailConfirmation(
+                fixtSendEmailConfirmationRequest
+            )
+        }
     }
 }
