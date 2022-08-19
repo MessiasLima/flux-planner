33b4bc06fd3643566947459daa79541f881c9155
messiaslima <messiaslima.03@gmail.com>
2022-08-19 09:10:43 +0100

    test: update UserDomainMapperTest

--- common/src/commonTest/kotlin/io/appoutlet/flux/common/domain/user/UserDomainMapperTest.kt
+++ common/src/commonTest/kotlin/io/appoutlet/flux/common/domain/user/UserDomainMapperTest.kt
@@ -2,6 +2,7 @@ package io.appoutlet.flux.common.domain.user
 
 import io.appoutlet.flux.common.core.database.generated.UserEntity
 import io.appoutlet.flux.common.core.network.authentication.AuthenticationResponse
+import io.appoutlet.flux.common.core.network.authentication.SignUpWithEmailResponse
 import io.appoutlet.flux.common.test.UnitTest
 import io.appoutlet.flux.common.util.toBoolean
 import kotlin.test.Test
@@ -36,5 +37,19 @@ class UserDomainMapperTest : UnitTest<UserDomainMapper>() {
         assertEquals(fixtUserEntity.idToken, actual.idToken)
         assertEquals(fixtUserEntity.refreshToken, actual.refreshToken)
         assertEquals(fixtUserEntity.registered.toBoolean(), actual.registered)
+    }
+
+    @Test
+    fun `should map from SignUpWithEmailResponse`(){
+        val fixtSignUpWithEmailResponse: SignUpWithEmailResponse = fixture()
+
+        val actual = sut.map(fixtSignUpWithEmailResponse)
+
+        assertEquals(fixtSignUpWithEmailResponse.localId, actual.id)
+        assertEquals(fixtSignUpWithEmailResponse.email, actual.email)
+        assertEquals("", actual.displayName)
+        assertEquals(fixtSignUpWithEmailResponse.idToken, actual.idToken)
+        assertEquals(fixtSignUpWithEmailResponse.refreshToken, actual.refreshToken)
+        assertEquals(false, actual.registered)
     }
 }
