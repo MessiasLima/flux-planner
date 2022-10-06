d044667f170a3fecf1550d849441ccdb404ad8a0
messiaslima <messiaslima.03@gmail.com>
2022-10-06 20:14:05 +0100

    feat: use home folder to store the database file

--- common/src/desktopMain/kotlin/io/appoutlet/flux/common/core/database/databasePlatformModule.kt
+++ common/src/desktopMain/kotlin/io/appoutlet/flux/common/core/database/databasePlatformModule.kt
@@ -4,12 +4,18 @@ import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 import io.appoutlet.flux.common.core.database.generated.FluxDB
 import org.koin.dsl.module
+import java.io.File
 
 // private const val DATABASE_PATH = "jdbc:sqlite:HOME/.config/flux/data/flux.db"
 
 actual val databasePlatformModule = module {
     single<SqlDriver> {
-        val driver = JdbcSqliteDriver("jdbc:sqlite:/home/messias/Workspace/flux.db")
+        val userHome = System.getProperty("user.home")
+        val databasePath = "$userHome/.config/flux/database"
+
+        File(databasePath).mkdirs()
+
+        val driver = JdbcSqliteDriver("jdbc:sqlite:$databasePath/flux.db")
         FluxDB.Schema.create(driver)
         driver
     }
